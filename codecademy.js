9b678300e1464c6bf438fd5ca90b40f7e4e36004
Paul <Paul@pauls-mbp.speedport_w_921v_1_36_000>
2015-03-28 11:55:41 +0100

    fixed the game, works now

--- codecademy.js
+++ codecademy.js
@@ -1,27 +1,43 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
-if (computerChoice >= 0 && computerChoice <= 0.34) {
+if (computerChoice < 0.34) {
     computerChoice = "rock";
-} else if(computerChoice >= 0.35 && computerChoice <= 0.67) {
+} else if (computerChoice <= 0.67) {
     computerChoice = "paper";
-} else {(computerChoice >= 0.68 && computerChoice <= 1)
+} else {
     computerChoice = "scissors";
 }
 
-var compare = function(userChoice, computerChoice) {
-    if(userChoice === computerChoice) {
+console.log("Computer: " + computerChoice);
+
+var compare = function( choice1, choice2)
+{
+    if (choice1 === choice2)
+    {
         return "The result is a tie!"
     }
-    else if (userChoice === "rock") {
-        if (computerChoice === "scissors") {
-            return("rock wins");
+    else if (choice1 === "rock")
+    {
+        if(choice2 === "scissors")
+        {
+            return "Rock wins";
+        }
+        else
+        {
+            return "Paper wins";
         }
     }
-    else if (userChoice === "paper") {
-        if (computerChoice === "rock") {
-            return ("paper wins");
+    else if (choice1 === "paper")
+    {
+        if(choice2 === "rock")
+        {
+            return "paper wins";
+        }
+        else
+        {
+            return "scissors wins";
         }
     }
 }
 
-compare(userChoice,computerChoice);
+console.log(compare(userChoice, computerChoice));
